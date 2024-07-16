de29b8624c0d25cc38c96f5418027185ce4f9056
alanb4rt <al4n.b4rt@gmail.com>
2024-07-16 16:27:34 +0200

    refacto of ListItem component to use destructuring for props

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { toRefs } from 'vue'
+
 const props = defineProps({
   item: Object
 })
+
+const { label, count, purchased, highPriority } = toRefs(props.item)
 </script>
 
 <template>
-  <li :class="{ strikeout: item.purchased, priority: item.highPriority }">
-    {{ item.count + ' ' + item.label }}
+  <li :class="{ strikeout: purchased, priority: highPriority }">
+    {{ count + ' ' + label }}
   </li>
 </template>
